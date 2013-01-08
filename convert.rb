39f032f1f6b804faae83dfd053a1e81747559661
Mayank Sharma <mayanks@gmail.com>
2013-01-08 12:01:01 +0530

    added image conversion code

--- convert.rb
+++ convert.rb
@@ -2,6 +2,22 @@
 
 require 'active_support/core_ext/hash/conversions'
 require 'date'
+require 'pathname'
+
+def convert_img(img,dim)
+  img_src = Pathname.new img.sub(/^\//,'').sub(/\?.*?$/,'')
+  img_dst = img_src.sub(img_src.extname,"_"+dim+img_src.extname)
+  `convert #{img_src.realpath} -trim -resize #{dim}^ -gravity Center -crop #{dim}+0+0  +repage #{img_dst}`
+  return "/#{img_dst}" 
+end
+
+def convert_230x150(img)
+  convert_img(img,"230x150")
+end
+
+def convert_thumbnail(img)
+  convert_img(img,"200x200")
+end
 
 def slug(title)
   _slug = title.gsub(/\W+/,'-')
@@ -14,27 +30,20 @@ def post_file_name(pub_date,title)
   "_posts/" + pub_date + "-" + slug(title) + ".markdown"
 end
 
+puts "Reading roma.xml"
 xmlData = File.read('roma.xml')
 
+puts "Parsing XML file"
 hash = Hash.from_xml(xmlData)
 
+puts "Extracting categories"
 categories = []
 hash["rss"]["channel"]["wp_category"].each do |cat|
   _cat = cat["wp_cat_name"]
   categories << _cat
-  _cat_slug = _cat.downcase.gsub(/\W+/,'-')
-  cat_filename = "category/#{_cat_slug}.markdown"
-  f = File.open(cat_filename,"w")
-  f.write <<EOF
----
-layout: category
-category: #{_cat}
-title: Category - #{_cat}
----
-EOF
-  f.close
 end
 
+puts "Processing Posts"
 hash["rss"]["channel"]["item"].each do |post|
   if post["wp_post_type"] == "post"
     pub_date_time = post["wp_post_date"]
@@ -61,6 +70,16 @@ hash["rss"]["channel"]["item"].each do |post|
     if content =~ /img .*src="(\S*)"/
       image_path = $~[1]
     end
+
+    if image_path and image_path =~ /^\//
+      image_230x150_path = convert_230x150(image_path)
+      image_thumbnail_path = convert_thumbnail(image_path)
+    end
+
+    image_path = "/images/default.jpg" unless image_path
+    image_230x150_path = "/images/default_230x150.jpg" unless image_230x150_path
+    image_thumbnail_path = "/images/default_thumbnail.jpg" unless image_thumbnail_path
+
     filename = post_file_name(pub_date,title)
     f = File.open(filename,"w")
     f.write <<EOF
@@ -69,6 +88,8 @@ permalink: #{permalink}
 layout: post
 title: #{title}
 image: #{image_path}
+image_230x150: #{image_230x150_path}
+image_thumbnail: #{image_thumbnail_path}
 published: true
 author: Roma Sharma
 categories: 
@@ -82,3 +103,4 @@ EOF
   end
 end
 
+puts "Done."
